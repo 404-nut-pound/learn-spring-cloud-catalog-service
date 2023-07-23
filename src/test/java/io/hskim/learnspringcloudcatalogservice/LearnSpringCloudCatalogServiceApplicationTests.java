14eb894e34d49dd3552927a9e364457e06140a0b
404-nut-pound <souls4523@gmail.com>
2023-07-23 22:01:59 +0900

    initial commit

---
+++ src/test/java/io/hskim/learnspringcloudcatalogservice/LearnSpringCloudCatalogServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package io.hskim.learnspringcloudcatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringCloudCatalogServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
